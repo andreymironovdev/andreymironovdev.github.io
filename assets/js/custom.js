5dca11bce48257bb5794a9ef2f86b2a949c399ad
and.v.mironov <and.v.mironov@tinkoff.ru>
2024-05-01 18:06:22 +0300

    change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,18 +19,6 @@
     }
   });
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var box = $(".header-text").height();
-    var header = $("header").height();
-
-    if (scroll >= box - header) {
-      $("header").addClass("background-header");
-    } else {
-      $("header").removeClass("background-header");
-    }
-  });
-
   // Mobile menu dropdown
   $(".submenu").on("click", function() {
     var width = $(window).width();
